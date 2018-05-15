3ed405a4560115276b313600d19d3c416df9ddce
Rafna Bash <rafnabash@gmail.com>
2018-05-15 20:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyRemoveFirstKitten(){
   return kittens
 }
 
-function appendKitten(name){
-  [...kittens, name]
+function appendKitten("Broom"){
+  [...kittens, "Broom"]
   return kittens
 }
